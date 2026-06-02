629de22a2c65ddf3a4cb832bf5f93aebeb7f2077
Ekaette Umoh <equmoh@gmail.com>
2026-06-02 13:30:57 -0400

    fix: add azurerm backend for persistent terraform state

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,6 +6,13 @@ terraform {
     }
   }
   required_version = ">= 1.3.0"
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stterraformstate18262"
+    container_name       = "tfstate"
+    key                  = "devsecops.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
